4032627552ab8dc7207b22adc4a689b8ea811f20
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2014-10-11 12:04:13 +0200

    Diverse for å få satt opp bokskap for produksjon

--- skapsystem/skapsystem/settings/production.py
+++ skapsystem/skapsystem/settings/production.py
@@ -1,3 +1,6 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from .base import *
 
 DEBUG = False
@@ -8,9 +11,18 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bokskap_nabla_no',
+        'USER': 'bokskap_nabla_no',
+        'PASSWORD': '',
+    }
+}
+
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemCachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
         'KEY_PREFIX': 'bokskap',
     }
