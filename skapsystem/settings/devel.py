a80d630023b08e1edcf18ecfda73f48e87e209aa
Thorvald <Thorvald.tb@gmail.com>
2017-10-04 19:05:52 +0200

    Changed ALLOWED_HOSTS in devel.py to include localhost and 127.0.0.1

--- skapsystem/settings/devel.py
+++ skapsystem/settings/devel.py
@@ -3,6 +3,8 @@ from .base import *
 DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ALLOWED_HOSTS = ["bokskap.nabla.no", "localhost", "127.0.0.1"]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
