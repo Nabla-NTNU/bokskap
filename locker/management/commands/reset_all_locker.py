8c771558bf7a4f87c811ab55cc88c3d92d721de5
Thorvald Molthe Ballestad <thorvalb@stud.ntnu.no>
2018-08-11 17:59:58 +0000

    Logging info when resetting lockers

--- locker/management/commands/reset_all_locker.py
+++ locker/management/commands/reset_all_locker.py
@@ -1,11 +1,15 @@
+import logging
+
 from django.core.management.base import BaseCommand
 from locker.models import Locker
 
+logger = logging.getLogger(_name_)
 
 class Command (BaseCommand):
-    help = "Avregistrere samtlige skap, og sender mail til alle brukere der deres skap kan registreres pÃ¥ nytt"
+    help = "Avregistrere samtlige skap, og sender mail til alle brukere der deres skap kan registreres på nytt"
 
     def handle(self, *args, **options):
         lockers = Locker.objects.all()
         for locker in lockers:
             locker.reset()
+            logger.info(f"Reset locker {locker.locker_number} in {locker.room}")
