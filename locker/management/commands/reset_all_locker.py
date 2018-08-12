5e81f8247569c486da8843f6d0c88bcc2b739220
Thorvald Ballestad <thorvald.tb@gmail.com>
2018-08-12 12:02:41 +0200

    Changes reset_all_locker command from being locker-based to being ownership-based

--- locker/management/commands/reset_all_locker.py
+++ locker/management/commands/reset_all_locker.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from locker.models import Locker
+from locker.models import Locker, Ownership
 
 logger = logging.getLogger(__name__)
 
@@ -9,9 +9,15 @@ class Command (BaseCommand):
     help = "Avregistrere samtlige skap, og sender mail til alle brukere der deres skap kan registreres på nytt"
 
     def handle(self, *args, **options):
-        lockers = Locker.objects.all()
-        for locker in lockers:
-            locker.reset()
-            logger.debug(f"Reset locker ({locker.room}, {locker.locker_number})")
-            
-        logger.info(f"Reset all lockers")
+        ownerships = Ownership.objects.filter(time_unreserved=None)
+        
+        if(len(ownerships) == 0):
+            logger.info("No ownerships to reset")
+            return
+
+        for ownership in ownerships:
+            ownership.reset()
+            logger.debug(f"Reset {ownership.locker}, which was owned by {ownership.user}")
+
+        logger.info(f"Reset {len(ownerships)} ownerships")
+       
