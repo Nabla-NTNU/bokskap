fdc01f099b207b8ff4446e1657c933971bd42661
Thorvald Ballestad <thorvald.tb@gmail.com>
2018-08-12 12:18:36 +0200

    Updates reset_all test

--- locker/tests/test_reset_all.py
+++ locker/tests/test_reset_all.py
@@ -2,17 +2,26 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core import mail
 from .fixture_factories import LockerFactory
+from random import sample
 
+
+NUMBER_OF_LOCKERS = 10
+NUMBER_OF_UNREGISTERED_LOCKERS = 4
 
 class TestResetAll(TestCase):
     def setUp(self):
-        self.lockers = LockerFactory.create_batch(10)
+        self.lockers = LockerFactory.create_batch(NUMBER_OF_LOCKERS)
 
     def test_reset_all(self):
+        # Create a database with some active ownerships and some inactive
+        
         for i, locker in enumerate(self.lockers):
             user = User.objects.create(username="username{}".format(i))
             locker.register(user)
             self.assertTrue(locker.is_registered())
+
+        for locker in sample(self.lockers, NUMBER_OF_UNREGISTERED_LOCKERS):
+            locker.reset()
 
         mail.outbox.clear()
 
@@ -20,4 +29,4 @@ class TestResetAll(TestCase):
             locker.reset()
             self.assertFalse(locker.is_registered())
 
-        self.assertEqual(len(mail.outbox), 10)
+        self.assertEqual(len(mail.outbox), NUMBER_OF_LOCKERS - NUMBER_OF_UNREGISTERED_LOCKERS)
